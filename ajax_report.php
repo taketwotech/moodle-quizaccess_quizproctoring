a2bde4ce202a078f6c27b119d3a803c017ac1911
jainkavita31 <ks@taketwotechnologies.com>
2023-04-06 17:56:51 +0530

    Github #51 previous button working mode

--- ajax_report.php
+++ ajax_report.php
@@ -11,6 +11,33 @@ $changestatus = optional_param('changestatus', false, PARAM_BOOL);
 $status = optional_param('status', '', PARAM_TEXT);
 $currentpage = optional_param('currentpage', 0 ,PARAM_INT);
 
+if ($currentpage) {
+    $offset = $currentpage * 20;
+    $sql = "select * from {quizaccess_proctor_data} where  (status != '' OR image_status = 'M') AND userid = ".$userid." AND quizid =". $quizid. " AND attemptid =". $attemptid." AND deleted = 0 " ;
+    $getImages = $DB->get_records_sql($sql, null, $offset , 20);
+
+    $imgarray= array();
+    $sqlcount = "select * from {quizaccess_proctor_data} where (status != '' OR image_status = 'M') AND userid = ".$userid." AND quizid =". $quizid. " AND attemptid =". $attemptid." AND deleted = 0 " ;
+    $countrecord  = $DB->get_records_sql($sqlcount, null);
+
+    if ($countrecord) {
+        $countrecord = ceil(count($countrecord) / 20);
+    }
+    foreach($getImages as $img) {
+       if (strlen($img->userimg) < 40) {
+            $quizobj = \quiz::create($img->quizid, $img->userid);
+            $context = $quizobj->get_context();
+            $fs = get_file_storage();
+            $f1 = $fs->get_file($context->id, 'quizaccess_quizproctoring', 'cameraimages', $img->id, '/', $img->userimg);
+            $target = $f1->get_content();
+        } else {
+            $target = $img->userimg;
+        }
+        array_push($imgarray, array('title' => $img->image_status == 'M' ? get_string('mainimage', 'quizaccess_quizproctoring') : get_string($img->status, 'quizaccess_quizproctoring', '') , 'img' => $target, 'totalpage' => $countrecord));
+    }
+    echo json_encode($imgarray);
+
+} else {
     $sql = "select * from {quizaccess_proctor_data} where  (status != '' OR image_status = 'M') AND userid = ".$userid." AND quizid =". $quizid. " AND attemptid =". $attemptid." AND deleted = 0 " ;
     $getImages = $DB->get_records_sql($sql, null, '' , 20);
     $imgarray= array();
@@ -32,4 +59,5 @@ $currentpage = optional_param('currentpage', 0 ,PARAM_INT);
         }
         array_push($imgarray, array('title' => $img->image_status == 'M' ? get_string('mainimage', 'quizaccess_quizproctoring') : get_string($img->status, 'quizaccess_quizproctoring', '') , 'img' => $target, 'totalpage' => $countrecord));
     }
-echo json_encode($imgarray);
+    echo json_encode($imgarray);
+}  
