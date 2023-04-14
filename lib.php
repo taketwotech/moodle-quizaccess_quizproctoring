2de69fb3132e8ccf4be2804ca6b19dae6042f9a6
jainkavita31 <ks@taketwotechnologies.com>
2023-04-14 16:33:54 +0530

    Update lib.php

--- lib.php
+++ lib.php
@@ -22,6 +22,7 @@
  * @copyright  2020 Mahendra Soni <ms@taketwotechnologies.com> {@link https://taketwotechnologies.com}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+defined('MOODLE_INTERNAL') || die();
 require_once($CFG->dirroot . '/mod/quiz/locallib.php');
 
 define('QUIZACCESS_QUIZPROCTORING_NOFACEDETECTED', 'nofacedetected');
