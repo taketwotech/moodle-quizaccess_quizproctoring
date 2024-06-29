0254b7ec7b0e32f1ed555473077a366917fdc512
jainkavita31 <ks@taketwotechnologies.com>
2024-06-29 14:35:27 +0530

    fix errors

--- amd/src/add_camera.js
+++ amd/src/add_camera.js
@@ -368,50 +368,58 @@ function($, str, ModalFactory) {
      * @param {function} callback - The callback function to execute after setting up the media stream.
      * @return {void}
      */
-     function setupLocalMedia(cmid, mainimage, verifyduringattempt, attemptid,
-        teacher, setinterval, serviceoption, quizid, callback) {
+    function setupLocalMedia(cmid, mainimage, verifyduringattempt, attemptid, teacher, setinterval, serviceoption, quizid, callback) {
         require(['core/ajax'], function() {
-        if (localMediaStream !== null) {
-            if (callback) {
-                callback();
+            if (localMediaStream !== null) {
+                if (callback) {
+                    callback();
+                }
+                return;
             }
-            return;
-        }
 
-        navigator.getUserMedia = (
-               navigator.getUserMedia ||
-               navigator.webkitGetUserMedia ||
-               navigator.mozGetUserMedia ||
-               navigator.msGetUserMedia);
+            navigator.getUserMedia = (
+                navigator.getUserMedia ||
+                navigator.webkitGetUserMedia ||
+                navigator.mozGetUserMedia ||
+                navigator.msGetUserMedia
+            );
 
-        attachMediaStream = function(element, stream) {
-            element.srcObject = stream;
-         };
+            attachMediaStream = function(element, stream) {
+                element.srcObject = stream;
+            };
 
             navigator.mediaDevices.getUserMedia({"audio": USE_AUDIO, "video": USE_VIDEO})
-            .then(function(stream) {
-            localMediaStream = stream;
-            var camera;
-            if (verifyduringattempt) {
-                var teacherroom = getTeacherroom();
-                if (teacherroom !== 'teacher') {
-                    $('<canvas>').attr({id: 'canvas', width: '280', height: '240', 'style': 'display: none;'}).appendTo('body');
-                    $('<video>').attr({
-                        'id': 'video',
-                        'class': 'quizaccess_quizproctoring-video',
-                        'width': '280',
-                        'height': '240',
-                        'autoplay': 'autoplay'}).appendTo('body');
-                    camera = new Camera(cmid, mainimage, attemptid, quizid);
-                    setInterval(camera.proctoringimage.bind(camera), setinterval * 1000);
-                }
-            }
-            if (callback) {
-                callback();
-            }
+                .then(function(stream) {
+                    localMediaStream = stream;
+                    var camera;
+                    if (verifyduringattempt) {
+                        var teacherroom = getTeacherroom();
+                        if (teacherroom !== 'teacher') {
+                            $('<canvas>').attr({id: 'canvas', width: '280', height: '240', 'style': 'display: none;'}).appendTo('body');
+                            $('<video>').attr({
+                                'id': 'video',
+                                'class': 'quizaccess_quizproctoring-video',
+                                'width': '280',
+                                'height': '240',
+                                'autoplay': 'autoplay'
+                            }).appendTo('body');
+                            camera = new Camera(cmid, mainimage, attemptid, quizid);
+                            setInterval(camera.proctoringimage.bind(camera), setinterval * 1000);
+                        }
+                    }
+                    return stream;
+                })
+                .then(function(stream) {
+                    if (callback) {
+                        callback();
+                    }
+                    return stream;
+                })
+                .catch(function(error) {
+                    throw error;
+                });
         });
-    });
-}
+    }
 
     /**
      * Get Teacher room
