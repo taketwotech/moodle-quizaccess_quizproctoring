53787630d0f77ce5a45635d94a6bd62c8ab40d7d
jainkavita31 <ks@taketwotechnologies.com>
2024-08-22 14:43:42 +0530

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -53,9 +53,9 @@ if (!$mainimage) {
     'image_status' => 'M',
     'attemptid' => $attemptid])) {
         $context = context_module::instance($cmid);
-        $fs = get_file_storage();print_r($context);die;
+        $fs = get_file_storage();
         $f1 = $fs->get_file($context->id, 'quizaccess_quizproctoring', 'cameraimages', $mainentry->id, '/', $mainentry->userimg);
-        $target = $f1->get_content();echo 'hii';die;
+        $target = $f1->get_content();
     }
 }
 $service = get_config('quizaccess_quizproctoring', 'serviceoption');
