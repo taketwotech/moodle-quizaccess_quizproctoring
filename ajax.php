6aa27d3a0d5cf715165a9d07fc8b6a9f24e68bf9
jainkavita31 <ks@taketwotechnologies.com>
2024-09-10 17:44:02 +0530

    Add eye close warning

--- ajax.php
+++ ajax.php
@@ -121,7 +121,10 @@ switch ($validate) {
         }
         break;
     case QUIZACCESS_QUIZPROCTORING_EYESNOTOPENED:
-        if ($mainimage) {
+        if (!$mainimage) {
+            quizproctoring_storeimage($img, $cmid, $attemptid,
+            $cm->instance, $mainimage, $service, QUIZACCESS_QUIZPROCTORING_EYESNOTOPENED);
+        } else {
             throw new moodle_exception(QUIZACCESS_QUIZPROCTORING_EYESNOTOPENED, 'quizaccess_quizproctoring', '', '');
         }
         break;
