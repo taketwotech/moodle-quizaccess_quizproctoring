3bea3bfd956fa338c3ada2cbbd1e127bacb28046
jainkavita31 <ks@taketwotechnologies.com>
2024-10-28 10:31:03 +0530

    revert url as we use old api

--- classes/api.php
+++ classes/api.php
@@ -92,7 +92,7 @@ class api {
     public static function proctor_image_api($imagedata, $userid, $quizid) {
         self::init();
         $curl = new \curl();
-        $url = 'https://proctor-dev.taketwotechnologies.com/validate';
+        $url = 'https://proctoring.taketwotechnologies.com/validate';
         $accesstoken = self::$accesstoken;
         $accesstokensecret = self::$accesstokensecret;
         $domain = self::domain();
