e79ad1c3a3990d3ffaebe86ad18081887b3ec8fd
jainkavita31 <ks@taketwotechnologies.com>
2025-01-22 16:11:41 +0530

    remove space

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025011801;
+$plugin->version = 2025011802;
 $plugin->requires = 2022041900;
 $plugin->release = 'v4.3.0';
 $plugin->maturity = 'MATURITY_STABLE';
