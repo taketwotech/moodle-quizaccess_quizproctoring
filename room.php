66beaf1dbf34bfcf1f9ec452954fc95fdaf77a1e
jainkavita31 <ks@taketwotechnologies.com>
2025-02-25 16:48:17 +0530

    add enableteacherproctor in rule file

--- room.php
+++ room.php
@@ -43,10 +43,10 @@ if ($proctorrecord->enableteacherproctor) {
 
     // Include js module.
     echo html_writer::script('',
-    	$CFG->wwwroot.'/mod/quiz/accessrule/quizproctoring/libraries/socket.io.js', true);
+    $CFG->wwwroot.'/mod/quiz/accessrule/quizproctoring/libraries/socket.io.js', true);
     $PAGE->requires->js_call_amd('quizaccess_quizproctoring/add_camera',
-'init', [$cmid, false, true, null, true, $room, $serviceoption,
-	$proctorrecord->enableteacherproctor]);
+    'init', [$cmid, false, true, null, true, $room, $serviceoption,
+    $proctorrecord->enableteacherproctor]);
     echo '<div id="nostudentonline">';
     echo get_string('nostudentonline', 'quizaccess_quizproctoring');
     echo '</div>';
--- rule.php
+++ rule.php
@@ -194,7 +194,7 @@ class quizaccess_quizproctoring extends quiz_access_rule_base {
         $proctoringdata = $DB->get_record('quizaccess_quizproctoring', ['quizid' => $this->quiz->id]);
         $PAGE->requires->js_call_amd('quizaccess_quizproctoring/add_camera',
             'init', [$this->quiz->cmid, true, false, $attemptid, false,
-                $this->quiz->id, $serviceoption, $securewindow->browsersecurity]);
+                $this->quiz->id, $serviceoption, $interval->enableteacherproctor, $securewindow->browsersecurity]);
         $element = $mform->addElement('static', 'proctoringmsg', '',
             get_string('notice', 'quizaccess_quizproctoring'));
         $element->setAttributes(['class' => 'proctoringmsg']);
