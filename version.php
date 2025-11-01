3f070893b1805c49f9af0d6b753374c2193e3501
jainkavita31 <ks@taketwotechnologies.com>
2025-11-01 11:10:28 +0530

    Update version.php

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2025110100;
 $plugin->requires = 2022041900;
-$plugin->release = 'v5.1';
+$plugin->release = 'v5.1.0';
 $plugin->maturity = 'MATURITY_STABLE';
 $plugin->component = 'quizaccess_quizproctoring';
