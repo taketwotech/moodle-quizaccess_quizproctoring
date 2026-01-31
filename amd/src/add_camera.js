392589d770675ed9d41996391f5c9dcf5b55e461
jainkavita31 <ks@taketwotechnologies.com>
2026-01-31 15:34:00 +0530

    revert last change

--- amd/src/add_camera.js
+++ amd/src/add_camera.js
@@ -751,7 +751,6 @@ function($, str, ModalFactory) {
                 if (callback) {
                     callback();
                 }
-                // eslint-disable-next-line promise/catch-or-return.
                 return;
             }
             var teacherroom = getTeacherroom();
