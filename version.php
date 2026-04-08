e7a634d589d4b53162d59b4cc40e0eae6d6bdc31
jainkavita31 <ks@taketwotechnologies.com>
2026-04-08 17:23:59 +0530

    language strings in albhabetical order

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026031000;
+$plugin->version = 2026040800;
 $plugin->requires = 2019111800;
-$plugin->release = 'v5.2';
+$plugin->release = 'v5.3';
 $plugin->maturity = 'MATURITY_STABLE';
 $plugin->component = 'quizaccess_quizproctoring';
